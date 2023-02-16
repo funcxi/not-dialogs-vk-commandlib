3015d550951ef25c1a4c7be7cc7875b58225a941
funcxi <124061465+funcxi@users.noreply.github.com>
2023-02-16 21:37:57 +0300

    Update MessageHandler.kt

--- src/main/kotlin/dev/sirias/commandlib/handler/MessageHandler.kt
+++ src/main/kotlin/dev/sirias/commandlib/handler/MessageHandler.kt
@@ -10,20 +10,38 @@ import java.util.*
 class MessageHandler(private val commandService: CommandService, private val commandPrefixes: Array<out Char>) {
     fun handleMessage(message: Message) {
         val commandArgs = message.text.lowercase().split(" ").toTypedArray()
+        val payload = if (message.replyMessage != null) {
+            fromJson(message.replyMessage.payload)
+        } else if (message.fwdMessages.isNotEmpty()) {
+            fromJson(message.fwdMessages[0].payload)
+        } else null
 
         val fromId = message.fromId
 
-        if (fromId > 0) return
+        if (payload != null && payload.has("user_id") && payload.get("user_id").asInt != fromId) return
 
-        val command = commandService.getCommand(commandArgs[0])?: return
+        val command = commandService.getCommand(
+            if (payload == null) {
+                if (commandPrefixes.any { commandArgs[0].startsWith(it) }) {
+                    commandArgs[0].substring(1)
+                } else null
+            } else {
+                payload.get("command").asString
+            }) ?: return
+
+        if (payload != null && !command.messageIds.containsValue(UUID.fromString(payload.get("unique_id")?.asString))) return
+        
+
         val modifyArgs = commandArgs.copyOfRange(1, commandArgs.size)
 
         val defaultMean = command.defaultMean
+        
 
         val subCommand = command.subCommands[modifyArgs[0]]
 
         if (subCommand == null) {
             defaultMean?.execute(command, message, modifyArgs, commandService)
+
             return
         }
 
